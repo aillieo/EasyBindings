14dfa30e4a6fb7ca6631c2567b79705e3a0dacf2
aillieo <aillieo@163.com>
2022-06-05 11:36:43 +0800

    update

--- EasyBindings/Scripts/Runtime/Binder.cs
+++ EasyBindings/Scripts/Runtime/Binder.cs
@@ -21,7 +21,7 @@ namespace AillieoUtils.EasyBindings
                 handles.Add(handle);
             }
         }
-        
+
         public void Dispose()
         {
             if (handles != null)
@@ -29,30 +29,30 @@ namespace AillieoUtils.EasyBindings
                 foreach (var handle in handles)
                 {
                     handle.Unlisten();
-                }   
+                }
             }
- 
+
             handles.Clear();
         }
-        
+
         public void Bind<T>(BindableProperty<T> bindableProperty, Action<PropertyChangedEventArg<T>> eventHandler)
         {
             IEventHandle handle = bindableProperty.onValueChanged.AddListener(eventHandler);
             Record(handle);
         }
-        
+
         public void Bind(BindableObject bindableObject, Action<string> eventHandler)
         {
-            IEventHandle handle = bindableObject.onValueChanged.AddListener(eventHandler);
+            IEventHandle handle = bindableObject.onPropertyChanged.AddListener(eventHandler);
             Record(handle);
         }
-        
+
         public void Bind<T>(Event<T> evt, Action<T> eventHandler)
         {
             IEventHandle handle = evt.AddListener(eventHandler);
             Record(handle);
         }
-        
+
         public void Bind(Event evt, Action eventHandler)
         {
             IEventHandle handle = evt.AddListener(eventHandler);
