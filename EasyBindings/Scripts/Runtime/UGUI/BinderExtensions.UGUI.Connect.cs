ccc32d60bd88b8f10091c1c46379b1ed5b603a08
aillieo <aillieo@163.com>
2023-02-05 17:00:50 +0800

    update

--- EasyBindings/Scripts/Runtime/UGUI/BinderExtensions.UGUI.Connect.cs
+++ EasyBindings/Scripts/Runtime/UGUI/BinderExtensions.UGUI.Connect.cs
@@ -23,7 +23,7 @@ namespace AillieoUtils.EasyBindings.UGUI
         /// <param name="toggle">Toggle to bind.</param>
         public static void Connect_ToggleIsOn(this Binder binder, BindableProperty<bool> bindableProperty, Toggle toggle)
         {
-            int depth = 0;
+            var depth = 0;
             binder.BindPropertyValue(bindableProperty, v => toggle.isOn = v);
             binder.BindUnityEvent(toggle.onValueChanged, v =>
             {
@@ -51,7 +51,7 @@ namespace AillieoUtils.EasyBindings.UGUI
         /// <param name="slider">Slider to bind.</param>
         public static void Connect_SliderValue(this Binder binder, BindableProperty<float> bindableProperty, Slider slider)
         {
-            int depth = 0;
+            var depth = 0;
             binder.BindPropertyValue(bindableProperty, v => slider.value = v);
             binder.BindUnityEvent(slider.onValueChanged, v =>
             {
@@ -79,7 +79,7 @@ namespace AillieoUtils.EasyBindings.UGUI
         /// <param name="inputField">InputField to bind.</param>
         public static void Connect_InputFieldText(this Binder binder, BindableProperty<string> bindableProperty, InputField inputField)
         {
-            int depth = 0;
+            var depth = 0;
             binder.BindPropertyValue(bindableProperty, v => inputField.text = v);
             binder.BindUnityEvent(inputField.onEndEdit, v =>
             {
@@ -107,7 +107,7 @@ namespace AillieoUtils.EasyBindings.UGUI
         /// <param name="scrollRect">ScrollRect to bind.</param>
         public static void Connect_ScrollRectValue(this Binder binder, BindableProperty<Vector2> bindableProperty, ScrollRect scrollRect)
         {
-            int depth = 0;
+            var depth = 0;
             binder.BindPropertyValue(bindableProperty, v => scrollRect.normalizedPosition = v);
             binder.BindUnityEvent(scrollRect.onValueChanged, v =>
             {
@@ -135,7 +135,7 @@ namespace AillieoUtils.EasyBindings.UGUI
         /// <param name="dropdown">Dropdown to bind.</param>
         public static void Connect_DropdownValue(this Binder binder, BindableProperty<int> bindableProperty, Dropdown dropdown)
         {
-            int depth = 0;
+            var depth = 0;
             binder.BindPropertyValue(bindableProperty, v => dropdown.value = v);
             binder.BindUnityEvent(dropdown.onValueChanged, v =>
             {
